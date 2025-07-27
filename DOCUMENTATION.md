f4541a5374e7472077f2e8c231c30462c6109a25
Apple <apple@Apples-MacBook-Pro.local>
2025-07-27 09:04:59 +0530

    Add put and delete

---
+++ DOCUMENTATION.md
@@ -0,0 +1,8 @@
+# 24 Jul
+Added basic setup
+BaseModel expects json from client side, annotates, validates it
+Learned 405 Method not allowed, 422 unproccessable Entit error(empty json body, incorrect format)
+
+# 26 Jul
+Added Query parameters, path parameters
+Put, Delete etc...
--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-
 app = FastAPI()
 
 class Item(BaseModel):
     name:str
     price:float
-
-
-
 
 @app.get("/")
 def read_root():
@@ -24,3 +20,20 @@ def read_name(name:str):
 @app.post("/items/")
 def create_item(item:Item):
     return {"name": item.name, "price": item.price}
+
+
+@app.get("/items")
+def get_items(limit: int = 10):
+    return {"message": f"returning {limit} items"}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id:int, item:Item):
+    return{"item_id": item_id, "updated":item}
+
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id:int):
+    return {"message":f"item {item_id} deleted"}
+
+
